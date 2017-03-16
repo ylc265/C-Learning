766dc70272111073fb4f350348c815fe56c70762
Daniel Chen <daniche@microsoft.com>
2017-03-15 17:22:26 -0700

    Async file

---
+++ Project1/Project1/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Class1
+    {
+        static void Main()
+        {
+//            FileAsyncWriter writer = new FileAsyncWriter(@"c:\Users\daniche\file1.txt");
+//            writer.Write("HI This is me");
+            ManualResetEvent[] events = new ManualResetEvent[3];
+            for (int i = 0; i < events.Length; i++)
+            {
+                events[i] = new ManualResetEvent(false);
+                FileAsyncWriter writer = new FileAsyncWriter(@"C:\Users\daniche\file" + i + ".txt", events[i]);
+                Thread t = new Thread(writer.Write);
+                t.Start("Hello This is Arnold");   
+            }
+            WaitHandle.WaitAll(events);
+            Console.WriteLine("Done");
+        }
+    }
+    
+}
---
+++ Project1/Project1/FileAsyncWriter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace Project1
+{
+    class FileAsyncWriter
+    {
+        private StreamWriter sw;
+        private ManualResetEvent ev;
+        public FileAsyncWriter(string path, ManualResetEvent ev) {
+            sw = File.CreateText(path);
+            this.ev = ev;
+        }
+        public void Write(object text)
+        {
+            sw.WriteLine((string)text);
+            sw.Flush();
+            ev.Set();
+        }
+    }
+}
